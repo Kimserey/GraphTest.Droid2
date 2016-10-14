d064707d2cda57e3f422c48b11a5b89d8cb78475
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-15 00:00:48 +0100

    working on 2.3.1.114

---
+++ BoxRendererTest/BoxRendererTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace BoxRendererTest
+{
+	public class Line
+	{
+		public float XStart { get; set; }
+		public float XStop { get; set; }
+		public float YStart { get; set; }
+		public float YStop { get; set; }
+	}
+
+	public class Padding
+	{
+		public float Left { get; set; }
+		public float Right { get; set; }
+		public float Top { get; set; }
+		public float Bottom { get; set; }
+	}
+
+	public class Data
+	{
+		public string X { get; set; }
+		public double Y { get; set; }
+	}
+
+	public class CustomCell : ViewCell
+	{
+		public CustomCell()
+		{
+			var layout = new AbsoluteLayout { Padding = new Thickness(20, 5) };
+			var x = new Label { HorizontalTextAlignment = TextAlignment.Start, HorizontalOptions = LayoutOptions.Start, VerticalTextAlignment = TextAlignment.Center };
+			var y = new Label { HorizontalTextAlignment = TextAlignment.End, HorizontalOptions = LayoutOptions.End, VerticalTextAlignment = TextAlignment.Center };
+			layout.Children.Add(x, new Rectangle(0, 0, .5, 1), AbsoluteLayoutFlags.All);
+			layout.Children.Add(y, new Rectangle(1, 0, .5, 1), AbsoluteLayoutFlags.All);
+			x.SetBinding(Label.TextProperty, "X");
+			y.SetBinding(Label.TextProperty, "Y");
+			this.View = layout;
+		}
+	}
+
+	public class Drawing : BoxView
+	{
+		public static readonly BindableProperty DataProperty =
+			  BindableProperty.Create(
+				  propertyName: "Data",
+				  returnType: typeof(IList<Data>),
+				  declaringType: typeof(Drawing),
+				  defaultValue:new List<Data>());
+
+		public IList<Data> Data
+		{
+			get { return (IList<Data>)GetValue(DataProperty); }
+			set { SetValue(DataProperty, value); }
+		}
+	}
+
+	public class App : Application
+	{
+		public App()
+		{
+			var list = new ListView
+			{
+				ItemsSource = new List<Data> {
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 },
+					new Data { X = "Hellow", Y = 100.05 }
+				},
+				ItemTemplate = new DataTemplate(typeof(CustomCell))
+			};
+
+			var layout = new AbsoluteLayout();
+
+			layout.Children.Add(new Drawing(), new Rectangle(0, 0, 1, .3), AbsoluteLayoutFlags.All);
+			layout.Children.Add(list, new Rectangle(0, 1, 1, .7), AbsoluteLayoutFlags.All);
+
+			var content = new ContentPage
+			{
+				Title = "Test",
+				Content = layout
+			};
+
+			MainPage = new NavigationPage(content);
+		}
+	}
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+
+using BoxRendererTest;
+using BoxRendererTest.Droid;
+
+[assembly: ExportRenderer(typeof(Drawing), typeof(DrawingRenderer))]
+namespace BoxRendererTest.Droid
+{
+	public class DrawingRenderer : BoxRenderer
+	{
+
+		Paint textPaint;
+		Paint axesPaint;
+		Paint bandsPaint;
+		Paint linePaint;
+		Paint markersPaint;
+		IList<Data> data;
+		Padding padding;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+		{
+			base.OnElementChanged(e);
+			Initialise();
+		}
+
+		protected override void OnDraw(Canvas canvas)
+		{
+			base.OnDraw(canvas);
+
+			DrawingRenderer.DrawChart(padding,
+						   canvas,
+						   linePaint,
+						   markersPaint,
+						   axesPaint,
+						   bandsPaint,
+						   textPaint,
+						   Resources.DisplayMetrics.Density,
+						   this.Width,
+						   this.Height,
+						   data);
+		}
+
+		static void DrawChart(
+			Padding padding,
+			Canvas canvas,
+			Paint linePaint,
+			Paint marketsPaint,
+			Paint axesPaint,
+			Paint bandsPaint,
+			Paint textPaint,
+			float density,
+			int viewWidth,
+			int viewHeight,
+			IEnumerable<Data> items)
+		{
+			var horizontal = new Line
+			{
+				XStart = padding.Left,
+				XStop = viewWidth - padding.Right,
+				YStart = viewHeight - padding.Bottom,
+				YStop = viewHeight - padding.Bottom
+			};
+
+
+			var vertical = new Line
+			{
+				XStart = padding.Left,
+				XStop = padding.Left,
+				YStart = padding.Top,
+				YStop = viewHeight - padding.Bottom
+			};
+
+			DrawXLabels(canvas, textPaint, horizontal, items.Select(i => i.X));
+			DrawYLabels(canvas, density, bandsPaint, textPaint, horizontal, vertical, items.Select(i => i.Y));
+			DrawPlot(canvas, density, linePaint, marketsPaint, textPaint, horizontal, vertical, items);
+
+			canvas.DrawLine(horizontal.XStart, horizontal.YStart, horizontal.XStop, horizontal.YStop, axesPaint);
+			canvas.DrawLine(vertical.XStart, vertical.YStart, vertical.XStop, vertical.YStop, axesPaint);
+
+		}
+
+		static void DrawXLabels(Canvas canvas, Paint textPaint, Line horizontal, IEnumerable<string> labels)
+		{
+			textPaint.TextAlign = Paint.Align.Left;
+
+			var sectionWidth = (horizontal.XStop - horizontal.XStart) / labels.Count();
+
+			foreach (Tuple<string, int> l in labels.Select((string l, int index) => Tuple.Create(l, index)))
+			{
+				var x = sectionWidth * (l.Item2 + 1f / 2f) + horizontal.XStart;
+				var halfedTextSize = textPaint.MeasureText(l.Item1) / 2f;
+
+				canvas.DrawText(
+					text: l.Item1,
+					x: x - halfedTextSize,
+					y: horizontal.YStart + textPaint.TextSize,
+					paint: textPaint);
+			}
+		}
+
+		static void DrawYLabels(Canvas canvas, float density, Paint bandsPaint, Paint textPaint, Line horizontal, Line vertical, IEnumerable<double> values)
+		{
+			textPaint.TextAlign = Paint.Align.Right;
+
+			var numberOfSections = (int)Math.Ceiling(values.Max() / 100);
+			var sectionWidth = (vertical.YStop - vertical.YStart) / numberOfSections;
+
+			foreach (var v in Enumerable.Range(0, numberOfSections).Select(i => Tuple.Create(i * 100, i)))
+			{
+				var y = vertical.YStop - sectionWidth * v.Item2;
+
+				canvas.DrawText(
+					text: v.Item1.ToString(),
+					x: vertical.XStart - 2 * density,
+					y: y - (textPaint.Ascent() / 2f),
+					paint: textPaint);
+
+				if (v.Item2 % 2 > 0 && v.Item2 < numberOfSections)
+					canvas.DrawRect(
+						left: horizontal.XStart,
+						top: y - sectionWidth,
+						right: horizontal.XStop,
+						bottom: y,
+						paint: bandsPaint);
+			}
+		}
+
+		static void DrawPlot(Canvas canvas, float density, Paint linePaint, Paint markersPaint, Paint valuePaint, Line horizontal, Line vertical, IEnumerable<Data> items)
+		{
+			var sectionWidth = (horizontal.XStop - horizontal.XStart) / items.Count();
+			var ceiling = (int)Math.Ceiling(items.Max(i => i.Y) / 100f) * 100f;
+			var points = new List<Tuple<float, float, double>>();
+
+			foreach (var l in items.Select((l, index) => Tuple.Create(l.X, l.Y, index)))
+			{
+				var x = sectionWidth * (l.Item3 + 1f / 2f) + horizontal.XStart;
+				var y = (float)l.Item2 * (vertical.YStop - vertical.YStart) / ceiling;
+
+				points.Add(Tuple.Create(x, vertical.YStop - y, l.Item2));
+			}
+
+			for (int i = 0; i < points.Count; i++)
+			{
+				if (i < points.Count - 1)
+					canvas.DrawLine(
+						points[i].Item1,
+						points[i].Item2,
+						points[i + 1].Item1,
+						points[i + 1].Item2,
+						linePaint);
+
+				canvas.DrawCircle(
+					cx: points[i].Item1,
+					cy: points[i].Item2,
+					radius: 5 * density,
+					paint: markersPaint);
+
+				canvas.DrawText(
+					text: points[i].Item3.ToString(),
+					x: points[i].Item1,
+					y: points[i].Item2,
+					paint: valuePaint);
+			}
+		}
+
+		void Initialise()
+		{
+			padding = new Padding
+			{
+				Left = 40 * Resources.DisplayMetrics.Density,
+				Right = 20 * Resources.DisplayMetrics.Density,
+				Bottom = 30 * Resources.DisplayMetrics.Density,
+				Top = 20 * Resources.DisplayMetrics.Density
+			};
+
+			textPaint = new Paint
+			{
+				TextSize = 14 * Resources.DisplayMetrics.Density,
+				Color = Color.ParseColor("#37474F")
+			};
+
+			axesPaint = new Paint
+			{
+				StrokeWidth = 2 * Resources.DisplayMetrics.Density,
+				Color = Color.ParseColor("#37474F")
+			};
+
+			linePaint = new Paint
+			{
+				StrokeWidth = 2 * Resources.DisplayMetrics.Density,
+				Color = Color.ParseColor("#FF5722")
+			};
+
+			markersPaint = new Paint
+			{
+				StrokeWidth = 3 * Resources.DisplayMetrics.Density,
+				Color = Color.ParseColor("#448AFF")
+			};
+
+			bandsPaint = new Paint
+			{
+				Color = Color.ParseColor("#EEEEEE")
+			};
+
+			data = new List<Data> {
+				new Data { X = "JAN", Y = 266.7 },
+				new Data { X = "FEB", Y = 250.4 },
+				new Data { X = "MAR", Y = 330 },
+				new Data { X = "JUN", Y = 126 },
+				new Data { X = "JUL", Y = 220 },
+				new Data { X = "AUG", Y = 230 },
+				new Data { X = "SEP", Y = 266 }
+			};
+		}
+	}
+
+	[Activity(Label = "BoxRendererTest.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
+		}
+	}
+}
