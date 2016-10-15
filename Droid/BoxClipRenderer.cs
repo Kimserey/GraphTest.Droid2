2f1c1cf9d6288c3ca2ccb093903f5ce638e0904f
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-15 22:00:20 +0100

    added some clipping examples

--- Droid/BoxClipRenderer.cs
+++ Droid/BoxClipRenderer.cs
@@ -21,11 +21,37 @@ using BoxRendererTest;
 [assembly: ExportRenderer(typeof(BoxClip), typeof(BoxClipRenderer))]
 namespace BoxRendererTest
 {
-	public class BoxClipRenderer: BoxRenderer
+	public class BoxClipRenderer : BoxRenderer
 	{
+		readonly Paint paint = new Paint();
+
 		protected override void OnDraw(Canvas canvas)
 		{
 			base.OnDraw(canvas);
+
+			var density = Resources.DisplayMetrics.Density;
+
+			// Draws a Green rectangle 100x100 at 0,0
+			paint.Color = Color.Green;
+			canvas.DrawRect(new RectF(0, 0, 100 * density, 100 * density), paint);
+
+			// Save canvas before executing clipping
+			// Clips canvas to only draw in 50x100 at 50,50
+			canvas.Save();
+			canvas.ClipRect(new RectF(50 * density, 50 * density, 100 * density, 150 * density));
+
+			// Draws a Red rectangle 100x100 at 50,50
+			// Only 50x100 is drawn because the canvas is previously clipped at 50x100
+			paint.Color = Color.Red;
+			canvas.DrawRect(new RectF(50 * density, 50 * density, 150 * density, 150 * density), paint);
+
+			// Restore canvas to before executing clipping
+			// Translate canvas to 150x100
+			// Draws a Blue rectangle 200x200 a 0,0 (translated)
+			canvas.Restore();
+			canvas.Translate(150 * density, 100 * density);
+			paint.Color = Color.Blue;
+			canvas.DrawRect(new RectF(0, 0, 200 * density, 200 * density), paint);
 		}
 	}
 }
