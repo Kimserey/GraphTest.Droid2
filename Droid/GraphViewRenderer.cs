91e27ce51bef723f1cc6441290f21caf1bc87205
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-16 18:09:01 +0100

    removed unused usings

--- Droid/GraphViewRenderer.cs
+++ Droid/GraphViewRenderer.cs
@@ -2,16 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel;
-
-using Android.App;
-using Android.Content;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
 using Android.Graphics;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Color = Android.Graphics.Color;
